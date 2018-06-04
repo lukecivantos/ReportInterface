4be4083b2215f006acd023de7faffd9a4544d0aa
Luke Civantos <lfcivantos@gmail.com>
2018-06-03 22:14:48 -0400

    First Commit

---
+++ flaskr/blog.py
@@ -0,0 +1,157 @@
+from flask import (
+    Flask, Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+#importing for secure file upload and conversion
+from flaskr.formatFile import createFile
+from werkzeug.utils import secure_filename
+import os
+
+from flaskr.auth import login_required
+from flaskr.db import get_db
+
+bp = Blueprint('blog', __name__)
+
+@bp.route('/')
+@login_required
+def index():
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('blog/index.html', posts=posts)
+
+"""
+Following code handles file uploads
+First lines check for filename security
+below then reports set up downloads and handle uploaded files
+"""
+UPLOAD_FOLDER = 'flaskr/uploads'
+ALLOWED_EXTENSIONS = set(['txt'])
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@bp.route('/reports', methods=('GET', 'POST'))
+@login_required
+def reports():
+    filename = ""
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    posts.append(filename)
+    return render_template('blog/reports.html', posts=posts)
+
+
+
+
+
+
+
+
+
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO post (title, body, author_id)'
+                ' VALUES (?, ?, ?)',
+                (title, body, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/create.html')
+
+
+
+def get_post(id, check_author=True):
+    post = get_db().execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+
+    if post is None:
+        abort(404, "Post id {0} doesn't exist.".format(id))
+
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+
+    return post
+
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE post SET title = ?, body = ?'
+                ' WHERE id = ?',
+                (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+
+    return render_template('blog/update.html', post=post)
+
+
+
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.index'))
---
+++ flaskr/formatFile.py
@@ -0,0 +1,90 @@
+import shutil
+
+def createFile():
+    
+
+
+
+
+
+
+
+w = raw_input("Name of Incoming File?\n")
+x = raw_input("CNP or CP?\n")
+y = raw_input("Whats the day?\n")
+z = raw_input("Whats the month?\n")
+
+w_words = w.split("/")
+w2 = w_words[3] + "/" + w_words[4]
+w2 = w2[:-1]
+
+
+
+name = "Desktop/" + x + "-" + y + "-" + z + "-2018.xls"
+
+shutil.copy2('template.txt', name)
+
+a = open(name, 'a')
+b = open(w2, 'r')
+
+text = b.read()
+lines = text.split("\r\n")
+words = [None]*len(lines)
+counter = 0
+for line in lines:
+    words[counter] = line.split("\t")
+    counter += 1
+
+for line in words:
+    if len(line) > 1:
+        if words.index(line) == 0:
+            a.write("Account\t")
+        elif line[8] == "HPT 170: Intermission Impossible":
+            if int(float(line[9])) % 32 == 0:
+                a.write("BOS WD\t")
+            elif int(float(line[9])) % 42 == 0:
+                a.write("BOS WE\t")
+            else:
+                a.write("group/donation\t")
+        elif line[8] == "Alumni Dinner 2018":
+            if int(float(line[9])) == 35:
+                a.write("ALUMNI DIN\t")
+            else:
+                a.write("probably dinner with donation?\t")
+        elif line[8] == "HPT 170: Man of the Year":
+            a.write("MOY\t")
+        elif line[8] == "HPT 170: Woman of the Year":
+            a.write("WOY\t")
+        elif line[8] == "HPT 170 Donors":
+            if int(float(line[9])) >= 2500 and int(float(line[9])) < 5000:
+                a.write("FR BFS\t")
+            elif int(float(line[9])) >= 1000 and int(float(line[9])) < 2500:
+                a.write("PATRON\t")
+            elif int(float(line[9])) >= 500 and int(float(line[9])) < 1000:
+                a.write("FR BEN\t")
+            elif int(float(line[9])) >= 250 and int(float(line[9])) < 500:
+                if int(float(line[9])) == 280:
+                    a.write("Probably FR Kickline\t")
+                else:
+                    a.write("FR SUP\t")
+            elif int(float(line[9])) >= 125 and int(float(line[9])) < 250:
+                if int(float(line[9])) == 125:
+                    a.write("FR PAT or BOWS\t")
+                else:
+                    a.write("FR PAT\t")
+        elif line[8] == "HPT 170 Alumni Memberships":
+            if int(float(line[9])) == 280:
+                a.write("FR Kickline\t")
+            elif int(float(line[9])) >= 125:
+                a.write("FR PAT\t")
+            else:
+                a.write("group/donation\t")
+        else:
+            a.write("group/donation\t")
+        a.write(line[4] + "\t")
+        a.write(line[9] + "\t")
+        a.write(line[13] + "\t")
+        a.write(line[14] + "\r\n")
+
+a.close()
+b.close()
